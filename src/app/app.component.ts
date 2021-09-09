19a21b55fe5c4859b9ee126974f8cc482dbd49e9
anhern <andrea.hernandez@daimler.com>
2021-09-09 13:37:39 +0200

    add flopvoter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flopvoter';
+  selectedIndex = -1;
+
+  onVoteClick(index){
+    if(this.isSelected(index)){
+      console.log('in if')
+      this.selectedIndex = -1
+    } else{
+      console.log('else')
+      this.selectedIndex = index;
+    }
+  }
+
+  isSelected(indexNum){
+    return this.selectedIndex === indexNum
+  }
+
+  anySelected(){
+    return this.selectedIndex !== -1
+  }
+
+  getButtonText(index){
+    if(this.isSelected(index)){
+      return "Unvote"
+    }else
+    return "Vote"
+  }
+
 }
